41f7585e17739966d78a43ce9e2fe04050c050b1
Tan Wei Jie <tanweijie123@gmail.com>
2021-01-06 18:46:55 +0800

    Add CheckVersion into app

--- src/main/java/project/MainApp.java
+++ src/main/java/project/MainApp.java
@@ -5,15 +5,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.jsoup.Jsoup;
 import project.files.Config;
 import project.ui.GetSource;
 import project.ui.UiPart;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class MainApp extends Application {
 
+    private static final String RELEASE_LINK = "https://tanwj.link/Download";
     private UiPart currentlyShowing;
 
     @FXML
@@ -24,6 +31,12 @@ public class MainApp extends Application {
 
     @FXML
     private Button btnNext;
+
+    @FXML
+    private Label lblVersionNo;
+
+    @FXML
+    private Button btnUpdateNow;
 
     @Override
     public void init() throws Exception {
@@ -44,6 +57,22 @@ public class MainApp extends Application {
         primaryStage.setTitle("Parse GPQ");
         primaryStage.show();
 
+        String ver = MainApp.class.getPackage().getImplementationVersion();
+        if (ver == null || ver.isBlank()) {
+            lblVersionNo.setText("<Unable to retrieve>");
+        } else {
+            lblVersionNo.setText(ver);
+            try {
+                String releaseVer = Jsoup.connect(RELEASE_LINK).execute().parse().select("blockquote").select("a").text();
+                if (!releaseVer.strip().equals(ver.strip())) {
+                    btnUpdateNow.setVisible(true);
+                }
+            } catch (Exception e) {
+                System.err.println("[ERROR] Unable to connect to download page.");
+                e.printStackTrace();
+            }
+        }
+
         GetSource getSource = new GetSource(this); //first screen is get source
         updatePaneTop(getSource);
 
@@ -63,6 +92,17 @@ public class MainApp extends Application {
 
     public void btnNextClick() {
         currentlyShowing.btnClickNext();
+    }
+
+    public void btnUpdateClick() {
+        new Thread(() -> {
+            try {
+                java.awt.Desktop.getDesktop().browse(new URL(RELEASE_LINK).toURI());
+            } catch (IOException | URISyntaxException e) {
+                System.err.println("[ERROR] Unable to launch browser to download page.");
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public void enableNextButton() {
